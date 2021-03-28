b0baf9ce125d86f8830d94fe1b6c3dc595ba5565
Shuyib <benmainye@gmail.com>
2021-03-28 07:46:32 +0300

    edit and other metrics: connected components, triangle count and label propagation.

--- reanalysis_2020/script/spark_network_analysis.py
+++ reanalysis_2020/script/spark_network_analysis.py
@@ -7,6 +7,7 @@ bash pyspark --packages graphframes:graphframes:0.6.0-spark2.3-s_2.11
 
 # import relevant packages
 from graphframes import *
+from pyspark.sql import functions as F
 from pyspark import SparkContext
 
 # load vertices and edges
@@ -21,7 +22,7 @@ g = GraphFrame(v, e)
 
 # define degree centrality 
 # find the total degree
-total_degree = g.degree
+total_degree = g.degrees
 in_degree = g.inDegrees
 out_degree = g.outDegrees
 
@@ -34,11 +35,50 @@ degree_cen.coalesce(1).write.csv("../data/popularity_scores.csv")
 
 # find influence in a network: page rank
 # adjust the restProbability and put a low maxIter: you may get better results with more iterations
-results = g.pageRank(resetProbability=0.15, maxIter = 20)
+results = g.pageRank(resetProbability=0.15, maxIter = 100)
 
 # find the most influential nodes with this 
 pageRankres = results.vertices.sort("pagerank", ascending=False)
 
 # store the results to disk
-pageRankres.coalesce(1).write.csv("/Users/aoi-rain/Desktop/pagerank_scores.csv")
+pageRankres.coalesce(1).write.csv("../data/pagerank_scores.csv")
 
+# cohesiveness of a specific group or your overall network
+# result not useful due to the nature of the graph
+result = g.triangleCount()
+(result.sort("count", ascending=False)
+.filter('count > 0')
+.show())
+
+# strongly connected components
+# adjust the max iterations 
+contCompS = g.stronglyConnectedComponents(maxIter=10)
+
+# sort by component, arrange by component that exists, use a custom aggregation function collect_list and rename id to pmid
+# see the full contents of the dataframe
+(contCompS.sort("component")
+.groupby("component")
+.agg(F.collect_list("id").alias("pmid"))
+.show(31,truncate=False))
+
+# store the results for later interpretation
+# copy the results to a file
+
+# connected components
+contComp = g.connectedComponents()
+(contComp.sort("component")
+.groupby("component")
+.agg(F.collect_list("id").alias("pmid"))
+.show(truncate=False))
+
+# label propagation 
+# finds community detection
+# subset to show first 30 results
+labelP = g.labelPropagation(maxIter=10)
+(labelP
+.sort("label")
+.groupby("label")
+.agg(F.collect_list("id"))
+.show(31,truncate=False))
+
+# copy paste results to a file
